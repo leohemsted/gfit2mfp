70d4eabba0c3d28abc28e69b669d1fe89343697b
Leo <leohemsted@gmail.com>
2015-06-14 17:54:58 +0100

    fixed a bug

--- gfit2mfp/utils/date_range.py
+++ gfit2mfp/utils/date_range.py
@@ -6,9 +6,9 @@ class DateRange(object):
 
     def __contains__(self, other):
         if isinstance(other, DateRange):
-            return self.start < other.start < self.end or self.start < other.end < self.end
+            return other.start <= self.end or self.start <= other.end
         elif isinstance(other, datetime.datetime):
-            return self.start < other < self.end
+            return self.start < other <= self.end
 
     def __repr__(self):
         return 'DateRange(start={0}, end={1})'.format(repr(self.start), repr(self.end))
