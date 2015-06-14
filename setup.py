266264c3a15ba2c260c732b37fb7095e9843235c
Leo <leohemsted@gmail.com>
2015-06-14 16:30:49 +0100

    easier to drop python 2 support than deal with unittest2 for now

--- setup.py
+++ setup.py
@@ -18,9 +18,6 @@ setup(
     license='MIT',
     classifiers=[
         'Development Status :: 1 - Planning',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
