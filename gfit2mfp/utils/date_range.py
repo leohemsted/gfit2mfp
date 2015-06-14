305eb279f189cc7f5b1244a602106e6a73ed45d3
Leo <leohemsted@gmail.com>
2015-06-14 18:24:15 +0100

    check intersect works with date ranges and added a docstring. maybe i shouldn't extend __contains__. hmmmm

--- gfit2mfp/utils/date_range.py
+++ gfit2mfp/utils/date_range.py
@@ -5,10 +5,22 @@ class DateRange(object):
         self.start, self.end = start, end
 
     def __contains__(self, other):
+        '''
+        `x in my_date_range` will return true if `x` intersects the date range. If x is a datetime
+        object, then it simply checks if it is in the middle. If x is another DateRange it checks
+        whether the two intersect. So say your ranges look like so:
+
+        my_date_range:      |------------|
+        x:          |-----------|
+        y:              |-------------------|
+        z:                       |----|
+
+        All three x, y and z will return True.
+        '''
         if isinstance(other, DateRange):
             return other.start <= self.end or self.start <= other.end
         elif isinstance(other, datetime.datetime):
-            return self.start < other <= self.end
+            return self.start <= other <= self.end
 
     def __repr__(self):
         return 'DateRange(start={0}, end={1})'.format(repr(self.start), repr(self.end))
--- tests/test_date_range.py
+++ tests/test_date_range.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime as dt
 
 import pytest
 
@@ -20,27 +20,50 @@ def test_create_date_range():
     assert obj.end == e
 
 @pytest.mark.parametrize('start, end, other_start, other_end',
+    # just use ints in this test because writing datetimes is hard work
     [
-    # the other date range starts after, ends after
-    (2, 4, 3, 5),
-    # starts before, ends before
-    (2, 4, 1, 3),
-    # starts before, ends after
-    (2, 4, 1, 5),
-    # starts before, ends the exact same time as this one starts
-    (2, 4, 1, 2),
-    # starts exactly when this one ends, ends after
-    (2, 4, 4, 5),
+        # the other date range starts after, ends after
+        (2, 4, 3, 5),
+        # starts before, ends before
+        (2, 4, 1, 3),
+        # starts before, ends after
+        (2, 4, 1, 5),
+        # starts before, ends the exact same time as this one starts
+        (2, 4, 1, 2),
+        # starts exactly when this one ends, ends after
+        (2, 4, 4, 5),
 
-    # fail cases
+        # fail cases
 
-    # other range starts after
-    pytest.mark.xfail((2, 4, 5, 6)),
-    # other range ends before
-    pytest.mark.xfail((2, 4, 0, 1))
-])
-def test_compare_date_ranges_contained(start, end, other_start, other_end):
+        # other range starts after
+        pytest.mark.xfail((2, 4, 5, 6)),
+        # other range ends before
+        pytest.mark.xfail((2, 4, 0, 1))
+    ]
+)
+def test_compare_date_ranges_intersect(start, end, other_start, other_end):
     obj = DateRange(start, end)
     other = DateRange(other_start, other_end)
+
+    assert other in obj
+
+
+
+@pytest.mark.parametrize('start, end, other',
+    [
+        # date is before
+        pytest.mark.xfail((dt(2000, 1, 5), dt(2000, 1, 10), dt(2000, 1, 1))),
+        # date is exactly equal to start
+        (dt(2000, 1, 5), dt(2000, 1, 10), dt(2000, 1, 5)),
+        # date is in the middle
+        (dt(2000, 1, 5), dt(2000, 1, 10), dt(2000, 1, 7)),
+        # date is exactly equal to end
+        (dt(2000, 1, 5), dt(2000, 1, 10), dt(2000, 1, 10)),
+        # date is after
+        pytest.mark.xfail((dt(2000, 1, 5), dt(2000, 1, 10), dt(2000, 1, 12)))
+    ]
+)
+def test_compare_date_ranges_contains_date(start, end, other):
+    obj = DateRange(start, end)
 
     assert other in obj
